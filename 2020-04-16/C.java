d599da9ec83a59001ab40eedeba87bd4f8dbbe85
Arief Zuhri <ariefzuhri72@gmail.com>
2020-04-20 21:13:30 +0700

    Create C.java

---
+++ 2020-04-16/C.java
@@ -0,0 +1,13 @@
+public class C extends KarakterKartun implements Penduduk, Pahlawan{
+    @Override
+    public void menyanyi() {}
+
+    @Override
+    public void ketawa() {}
+
+    @Override
+    public void terbang() {}
+
+    @Override
+    public void berlari() {}
+}
