56b847884cf162186b061c15ecb9694b61c6ac29
Arief Zuhri <ariefzuhri72@gmail.com>
2020-04-20 21:13:03 +0700

    Create B.java

---
+++ 2020-04-16/B.java
@@ -0,0 +1,13 @@
+public class B extends KarakterKartun implements Pahlawan {
+    @Override
+    public void menyanyi() {}
+
+    @Override
+    public void ketawa() {}
+
+    @Override
+    public void terbang() {}
+
+    @Override
+    public void berlari() {}
+}
