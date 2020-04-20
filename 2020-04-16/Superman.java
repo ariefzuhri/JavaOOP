61798df23d54d924229061acddec962547a6b836
Arief Zuhri <ariefzuhri72@gmail.com>
2020-04-20 21:12:15 +0700

    Create Superman.java

---
+++ 2020-04-16/Superman.java
@@ -0,0 +1,21 @@
+public class Superman extends KarakterKartun implements Pahlawan{
+    @Override
+    public void menyanyi() {
+        System.out.println("La La La");
+    }
+
+    @Override
+    public void ketawa() {
+        System.out.println("Ha Ha Ha");
+    }
+
+    @Override
+    public void terbang() {
+        System.out.println("Terbang");
+    }
+
+    @Override
+    public void berlari() {
+        System.out.println("Berlari");
+    }
+}
